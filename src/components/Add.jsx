4d7c48f5e829833d9077e15f55d3118b0223b064
Aswin-krishnam <aswinkrishnam16@gmail.com>
2024-06-08 01:32:50 +0530

    Partial

---
+++ src/components/Add.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Add = () => {
+  return (
+    <div>
+            <Navbar />
+            <center>
+                <h1>Add Student</h1>
+            </center>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">User Id</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Id</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Title</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Completed</label>
+                                <input type="checkbox" name="" id="" className="form-control" />
+
+                            </div>
+                           
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <button className="btn btn-success">Add</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+  )
+}
+
+export default Add
